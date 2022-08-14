2af900c0fe3ba9f10a3ea2f4c92b0ea0558249cf
BradySmithCode <bradysmithcoding@gmail.com>
2022-08-14 17:34:07 -0230

    Organized code

--- src/Game.java
+++ src/Game.java
@@ -33,7 +33,7 @@ public class Game {
 
     public static String gameStartInterface(int difficulty, String word, ArrayList<String> wordsUsed) {
         while (difficulty == 0 & word.equals("noWords")) {
-            Scanner difficultyInput = new Scanner(System.in);
+
             System.out.println("-------------------------");
             System.out.println("Welcome to Bradys Hangman!");
             System.out.println("Please pick a difficulty");
@@ -42,31 +42,43 @@ public class Game {
             System.out.println("3. Hard");
             System.out.println("4. Quit");
             System.out.println("-------------------------");
+
+            Scanner difficultyInput = new Scanner(System.in);
             String difficultyResponse = difficultyInput.nextLine();
+
             if (difficultyResponse.equals("1")) {
+
                 GameSounds.clickSound();
                 word = WordPicker.pickWord(1, wordsUsed);
+
                 if (word.equals("noWords")) {
                     difficulty = 0;
                     System.out.println("No more words for this difficulty");
-                } else {
+                }
+                else {
                     wordsUsed.add(word);
                     break;
                 }
-            } else if (difficultyResponse.equals("2")) {
+            }
+            else if (difficultyResponse.equals("2")) {
+
                 GameSounds.clickSound();
                 word = WordPicker.pickWord(2, wordsUsed);
+
                 if (word.equals("noWords")) {
                     difficulty = 0;
                     System.out.println("No more words for this difficulty");
-                } else {
+                }
+                else {
                     wordsUsed.add(word);
                     break;
 
                 }
-            } else if (difficultyResponse.equals("3")) {
+            }
+            else if (difficultyResponse.equals("3")) {
                 GameSounds.clickSound();
                 word = WordPicker.pickWord(3, wordsUsed);
+
                 if (word.equals("noWords")) {
                     difficulty = 0;
                     System.out.println("No more words for this difficulty");
@@ -76,20 +88,27 @@ public class Game {
                     break;
                 }
 
-            }  else if (difficultyResponse.equals("4")) {
+            }
+            else if (difficultyResponse.equals("4")) {
+
                 System.exit(1);
             }
             else {
+
                 GameSounds.clickSound();
                 System.out.println("Incorrect input please try again.");
+
             }
         }
+
         return word;
 
     }
 
     public static ArrayList<Character> guessingLayout(String word) {
+
         ArrayList<Character> wordGuessLayout = new ArrayList<>();
+
         for (int i = 0; i < word.length(); i++) {
             wordGuessLayout.add(word.charAt(i));
         }
@@ -98,16 +117,18 @@ public class Game {
     }
 
     public static void playingGame(int lives, ArrayList<String> lettersUsed, ArrayList<Character> wordGuessLayout, String word, ArrayList<String> wordsUsed, int allWords) {
+
         ArrayList<Character> unknownLetters = (ArrayList<Character>) wordGuessLayout.clone();
         Collections.fill(unknownLetters, '_');
+
         while (lives != 0) {
             boolean wordCheck = false;
             boolean guessedWord = false;
+
             String letterLayout = "";
             System.out.println("-------------------------");
             System.out.println("Lives left: " + lives);
             System.out.println(LivesDrawer.livesOutput(lives));
-
 
             System.out.println(lettersUsed);
 
@@ -119,12 +140,15 @@ public class Game {
 
             while(true) {
                 Scanner guessInput = new Scanner(System.in);
+
                 System.out.println();
                 System.out.println("1. Guess a letter");
                 System.out.println("2. Guess a word");
                 System.out.println("3. Quit");
+
                 String choice = guessInput.nextLine();
                 if (choice.equals("1")) {
+
                     GameSounds.clickSound();
                     System.out.println("Type a letter to guess!");
                     String guess = guessInput.nextLine();
@@ -139,6 +163,7 @@ public class Game {
                         System.out.println(LivesDrawer.livesOutput(lives));
                         System.out.println(letterLayout);
                     }
+
                     else if (lettersUsed.contains(guess.toLowerCase())) {
                         System.out.println("Letter already used try again");
                         System.out.println("-------------------------");
@@ -146,14 +171,16 @@ public class Game {
                         System.out.println(LivesDrawer.livesOutput(lives));
                         System.out.println(letterLayout);
 
-                    } else if (guess.length() > 1 | guess.length() == 0) {
+                    }
+                    else if (guess.length() > 1 | guess.length() == 0) {
                         System.out.println("Please enter 1 letter");
                         System.out.println("-------------------------");
                         System.out.println("Lives left: " + lives);
                         System.out.println(LivesDrawer.livesOutput(lives));
                         System.out.println(letterLayout);
 
-                    } else {
+                    }
+                    else {
                         for (int j = 0; j < wordGuessLayout.size(); j++) {
                             Character letter = word.charAt(j);
 
@@ -169,7 +196,8 @@ public class Game {
                             GameSounds.loseSound();
                             System.out.println("Letter is not in word. Try again");
                             lives -= 1;
-                        } else {
+                        }
+                        else {
                             GameSounds.winSound();
                         }
 
@@ -177,7 +205,8 @@ public class Game {
                         break;
                     }
 
-                } else if (choice.equals("2")) {
+                }
+                else if (choice.equals("2")) {
                     GameSounds.clickSound();
                     System.out.println("Guess Word");
                     String wordGuess = guessInput.nextLine();
@@ -196,17 +225,20 @@ public class Game {
                         GameSounds.winSound();
                         System.out.println("You got the word!");
                         break;
-                    } else {
+                    }
+                    else {
                         System.out.println("Word guess is not correct try again");
                         lives -= 1;
                         GameSounds.loseSound();
                         break;
                     }
 
-                } else if (choice.equals("3")) {
+                }
+                else if (choice.equals("3")) {
                     GameSounds.clickSound();
                     System.exit(1);
-                } else {
+                }
+                else {
                     GameSounds.clickSound();
                     System.out.println("Invalid input please try again");
                     System.out.println("-------------------------");
@@ -224,13 +256,17 @@ public class Game {
             }
         }
         if (lives == 0) {
+
             System.out.println("-------------------------");
             System.out.println(LivesDrawer.livesOutput(lives));
             System.out.println("YOU LOST");
             System.out.println("Word was: " + word);
+
         } else {
+
             System.out.println();
             System.out.println("You won!");
+
         }
         if(WordPicker.pickWord(3, wordsUsed).equals("noWords") & WordPicker.pickWord(2, wordsUsed).equals("noWords") & WordPicker.pickWord(1, wordsUsed).equals("noWords")) {
             System.out.println("No words left, please restart game.");
